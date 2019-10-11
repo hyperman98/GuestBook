456216090a555773712050fb3d3cdfd05dd4245b
guest <guestbook@mail.ru>
2019-10-11 19:18:46 +0500

    Новый репозиторий

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCUMENT html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form method="post">
+            <label for="name">Имя</label><input type="text" name="name"/><br>
+            <label for="email">Email</label><input type="text" name="email"/><br>
+            <label for="text">Текст сообщения</label><textarea name="text"></textarea>
+        </form>
+    </body>
+</html>
